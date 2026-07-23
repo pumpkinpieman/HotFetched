a35bde0d57772d20cd3cbfa78f520ca5fd0e75b6
PumpkinPieMan <bloodthirstycheeseburger@gmail.com>
2026-07-23 03:00:58 -0400

    Version 3.4.0
    - Fixed issue with audio placement
    - Fixed custom boot loader image stuck in loop
    - Fixed compilation issues
    - Added new boards!
    - Fixed UI issues
    - Added Power on sound section, new you can get jiggy wid it!
    - Love you

--- webroot/bambu/cli_import.php
+++
@@ -1,4 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__.'/lib/ResearchDb.php';require_once __DIR__.'/lib/FirmwareImporter.php';
-if(PHP_SAPI!=='cli'||$argc<2){fwrite(STDERR,"Usage: php cli_import.php firmware.zip [notes]\n");exit(2);} $private=getenv('PRIVATE_DIR')?:dirname(__DIR__,2).'/private';$db=ResearchDb::open($private);$id=(new FirmwareImporter($db,$private))->import($argv[1],basename($argv[1]),$argv[2]??'CLI import');echo "Imported package #$id\n";
